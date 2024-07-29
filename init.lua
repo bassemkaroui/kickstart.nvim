63c67bb6423be177c9e4207b7baeebd8ce32cd6b
Bassem Karoui <bassem.karoui1@gmail.com>
2024-07-29 16:03:34 +0200

    added vim-tmux-runner plugin and added support for hidden files in telescope

--- init.lua
+++ init.lua
@@ -185,7 +185,7 @@ vim.keymap.set('i', '<A-l>', '<Right>', { noremap = true, silent = true }) --<CU
 vim.keymap.set('i', '<A-h>', '<Left>', { noremap = true, silent = true }) --<CUSTOM CHANGE>
 vim.keymap.set('i', '<A-j>', '<C-o>gj', { noremap = true, silent = true }) --<CUSTOM CHANGE>
 vim.keymap.set('i', '<A-k>', '<C-o>gk', { noremap = true, silent = true }) --<CUSTOM CHANGE>
-vim.keymap.set('n', '<leader>ts', function()
+vim.keymap.set('n', '<leader>tS', function()
   if vim.o.laststatus == 3 then
     vim.o.laststatus = 2
   else
@@ -413,6 +413,12 @@ require('lazy').setup({
             require('telescope.themes').get_dropdown(),
           },
         },
+
+        pickers = {
+          find_files = { find_command = { 'fd', '--type', 'f', '--color', 'never', '--hidden', '--exclude', '.git' } },
+          live_grep = { additional_args = { '--hidden', '--glob', '!.git' } },
+          grep_string = { additional_args = { '--hidden', '--glob', '!.git' } },
+        },
       }
 
       -- Enable Telescope extensions if they are installed
@@ -423,10 +429,25 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files { hidden = true, follow = true }
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sw', function()
+        builtin.grep_string {
+          additional_args = function(opts)
+            return { '--follow' }
+          end,
+        }
+      end, { desc = '[S]earch current [W]ord' })
+
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep {
+          additional_args = function(opts)
+            return { '--follow' }
+          end,
+        }
+      end, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -717,4 +717,27 @@ return {
       { '<leader>tm', '<CMD>MarkdownPreviewToggle<CR>', desc = 'Toggle markdown preview' },
     },
   },
+  {
+    'christoomey/vim-tmux-runner',
+    enabled = true and os.getenv 'TMUX' ~= nil,
+    event = 'VeryLazy',
+    keys = {
+      { '<leader>tC', '<cmd>VtrClearRunner<cr>', desc = 'Clear Tmux Runner' },
+      { '<leader>tF', '<cmd>VtrFocusRunner<cr>', desc = 'Focus Tmux Runner' },
+      { '<leader>tR', '<cmd>VtrReorientRunner<cr>', desc = 'Reorient Tmux Runner' },
+      { '<leader>ta', '<cmd>VtrReattachRunner<cr>', desc = 'Reattach Tmux Runner' },
+      { '<leader>tc', '<cmd>VtrFlushCommand<cr>', desc = 'Flush Tmux Runner Command' },
+      { '<leader>tf', '<cmd>VtrSendFile<cr>', desc = 'Send File to Tmux Runner' },
+      { '<leader>tk', '<cmd>VtrKillRunner<cr>', desc = 'Kill Tmux Runner' },
+      { '<leader>tl', '<cmd>VtrSendLinesToRunner<cr>', 'desc = Send Lines to Tmux Runner' },
+      { '<leader>to', "<cmd>VtrOpenRunner {'orientation': 'h', 'percentage': 50}<cr>", desc = 'Open Tmux Runner' },
+      { '<leader>tr', '<cmd>VtrResizeRunner<cr>', desc = 'Resize Tmux Runner' },
+      { '<leader>ts', '<cmd>VtrSendCommandToRunner<cr>', desc = 'Send Command to Tmux Runner' },
+    },
+    config = function()
+      vim.g.VtrStripLeadingWhitespace = 0
+      vim.g.VtrClearEmptyLines = 0
+      vim.g.VtrAppendNewline = 1
+    end,
+  },
 }
